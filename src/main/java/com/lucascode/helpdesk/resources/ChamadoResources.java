00b54face3684a20475572cf9eb2f4e69fabc054
lucasbarroscode <lucasbabezerra@gmail.com>
2023-12-07 10:15:30 -0300

    update para chamados

--- src/main/java/com/lucascode/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/lucascode/helpdesk/resources/ChamadoResources.java
@@ -43,6 +43,13 @@ public class ChamadoResources {
         return ResponseEntity.created(uri).build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+        Chamado newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+    }
+
+
 
 
 }
