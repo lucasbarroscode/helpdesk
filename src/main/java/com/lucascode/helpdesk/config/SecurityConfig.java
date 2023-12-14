134ac4493762a12a5e8db0c43d867b3ac715780d
lucasbarroscode <lucasbabezerra@gmail.com>
2023-12-14 15:13:40 -0300

    autorizacao com jwt

--- src/main/java/com/lucascode/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/lucascode/helpdesk/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.lucascode.helpdesk.config;
 
+import com.lucascode.helpdesk.security.JWTAuthenticationFilter;
+import com.lucascode.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,6 +25,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -33,6 +41,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //desabilitando csrf
                 .and().csrf().disable();
 
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS)
                 .permitAll()
@@ -40,6 +51,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
---
+++ src/main/java/com/lucascode/helpdesk/security/JWTAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.lucascode.helpdesk.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lucascode.helpdesk.domain.dto.CrendenciasDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    private JWTUtil jwtUtils;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            CrendenciasDTO creds = new ObjectMapper().readValue(request.getInputStream(), CrendenciasDTO.class);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            return authentication;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String username = ((UserSS) authResult.getPrincipal()).getUsername();
+        String token = jwtUtils.generateToken(username);
+
+        response.setHeader("access-control-expose-headers", "Authorization");
+        response.setHeader("Authorization", "Bearer " + token);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(401);
+        response.setContentType("application/json");
+        response.getWriter().append(json());
+    }
+
+    private CharSequence json() {
+        long date = new Date().getTime();
+        return "{"
+                + "\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+    }
+}
